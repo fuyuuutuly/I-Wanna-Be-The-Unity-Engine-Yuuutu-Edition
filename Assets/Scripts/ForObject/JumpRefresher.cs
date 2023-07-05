b2064aefd266e0a1e240aae3c3efbe2e39c81b6d
fuyuuutuly <46482658+fuyuuutuly@users.noreply.github.com>
2023-07-05 21:54:09 +0900

    jumpRefresherを動かすようにした

--- Assets/Scripts/ForObject/JumpRefresher.cs
+++ Assets/Scripts/ForObject/JumpRefresher.cs
@@ -6,10 +6,40 @@ public class JumpRefresher : MonoBehaviour
 {
     public int refreshTime = 100;
 
-    int timer = -1;
+    private int timer = -1;
+    private int mode = 0;
+    private float yStart;
+
+    private void Start()
+    {
+        yStart = transform.position.y;
+    }
 
     private void Update()
     {
+        if (mode == 0)
+        {
+            if (transform.position.y < yStart + 4)
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y + 0.5f);
+            }
+            else
+            {
+                mode = 1;
+            }
+        }
+        else
+        {
+            if (transform.position.y > yStart - 4)
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y - 0.5f);
+            }
+            else
+            {
+                mode = 0;
+            }
+        }
+
         if (GetComponent<SpriteRenderer>().enabled)
         {
             var player = GetComponent<PixelPerfectCollider>().InstancePlace(transform.position.x, transform.position.y, "Player");
